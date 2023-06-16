cdb6ecfb666f772cb2294ea170aec4bf28fa4c62
Jerome Cheviet <jcheviet@hotmail.com>
2023-06-16 23:19:51 +0200

    ADD - Test for service class.

--- microservice-user/src/main/java/com/mediscreen/muser/service/UserServiceImpl.java
+++ microservice-user/src/main/java/com/mediscreen/muser/service/UserServiceImpl.java
@@ -1,19 +1,25 @@
 package com.mediscreen.muser.service;
 
+import com.mediscreen.muser.exception.UserNotFoundException;
 import com.mediscreen.muser.model.User;
 import com.mediscreen.muser.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public Iterable<User> getUsers() {
+        log.debug("Get all users");
+        Iterable<User> users = userRepository.findAll();
+        if (users.iterator().next() == null) {
+            throw new UserNotFoundException("No user found in database");
+        }
+        return users;
     }
 }
