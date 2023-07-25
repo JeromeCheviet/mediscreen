c6b5895d5d667eff7548a8cd24f0be47f0ad4a71
Jerome Cheviet <jcheviet@hotmail.com>
2023-07-25 21:05:19 +0200

    UPDATE - Create model and service class. Data are actually implemented into the service class.

---
+++ microservice-clientui/src/app/models/patient.model.ts
@@ -0,0 +1,9 @@
+export class Patient {
+  patientId!: number;
+  family!: string;
+  given!: string;
+  dob!: string;
+  sex!: string;
+  address?: string;
+  phone?: string;
+}
---
+++ microservice-clientui/src/app/services/patient.service.ts
@@ -0,0 +1,58 @@
+import {Injectable} from "@angular/core";
+import {Patient} from "../models/patient.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+  patients: Patient[] = [
+    {
+      "patientId": 1,
+      "family": "TestNone",
+      "given": "Test",
+      "dob": "1966-12-31",
+      "sex": "F",
+      "address": "1 Brookside St",
+      "phone": "100-222-3333"
+    },
+    {
+      "patientId": 2,
+      "family": "TestBorderline",
+      "given": "Test",
+      "dob": "1945-06-24",
+      "sex": "M",
+      "address": "2 High St",
+      "phone": "200-333-4444"
+    },
+    {
+      "patientId": 3,
+      "family": "TestInDanger",
+      "given": "Test",
+      "dob": "2004-06-18",
+      "sex": "M",
+      "address": "3 Club Road",
+      "phone": "300-444-5555"
+    },
+    {
+      "patientId": 4,
+      "family": "TestEarlyOnset",
+      "given": "Test",
+      "dob": "2002-06-28",
+      "sex": "F",
+      "address": "4 Valley Dr",
+      "phone": "400-555-6666"
+    }
+  ];
+
+  getAllPatients(): Patient[] {
+    return this.patients;
+  }
+
+  getPatientById(patientID: number): Patient {
+    const patient = this.patients.find(patient => {
+      patient.patientId === patientID
+    });
+    if (!patient) throw new Error('Patient not found');
+    return patient;
+  }
+}
