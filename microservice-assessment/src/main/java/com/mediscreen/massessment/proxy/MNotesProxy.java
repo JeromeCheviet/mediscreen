69a20817991a73e161ba4ca20606580dc0f83186
Jerome Cheviet <jcheviet@hotmail.com>
2023-11-10 12:06:00 +0100

    UPDATE - Handling error when request not found data

--- microservice-assessment/src/main/java/com/mediscreen/massessment/proxy/MNotesProxy.java
+++ microservice-assessment/src/main/java/com/mediscreen/massessment/proxy/MNotesProxy.java
@@ -1,13 +1,14 @@
 package com.mediscreen.massessment.proxy;
 
 import com.mediscreen.massessment.bean.NoteBean;
+import com.mediscreen.massessment.configuration.CustomFeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-notes", url = "localhost:8082")
+@FeignClient(name = "microservice-notes", url = "localhost:8082", configuration = CustomFeignClientConfiguration.class)
 public interface MNotesProxy {
     @GetMapping(value = "/patHistory/{patId}")
     List<NoteBean> getPatHistory(@PathVariable("patId") int patId);
--- microservice-assessment/src/main/java/com/mediscreen/massessment/proxy/MPatientProxy.java
+++ microservice-assessment/src/main/java/com/mediscreen/massessment/proxy/MPatientProxy.java
@@ -1,11 +1,12 @@
 package com.mediscreen.massessment.proxy;
 
 import com.mediscreen.massessment.bean.PatientBean;
+import com.mediscreen.massessment.configuration.CustomFeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "microservice-patient", url = "localhost:8081")
+@FeignClient(name = "microservice-patient", url = "localhost:8081", configuration = CustomFeignClientConfiguration.class)
 public interface MPatientProxy {
     @GetMapping(value = "/patient/{id}")
     PatientBean getPatient(@PathVariable("id") int id);
