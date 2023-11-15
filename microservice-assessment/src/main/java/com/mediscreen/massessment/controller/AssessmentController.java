d3df371dfe8788d2415b5162881d7cd675015b73
Jerome Cheviet <jcheviet@hotmail.com>
2023-11-15 14:15:31 +0100

    UPDATE - now controller return patient assessment

--- microservice-assessment/src/main/java/com/mediscreen/massessment/controller/AssessmentController.java
+++ microservice-assessment/src/main/java/com/mediscreen/massessment/controller/AssessmentController.java
@@ -2,8 +2,10 @@ package com.mediscreen.massessment.controller;
 
 import com.mediscreen.massessment.bean.NoteBean;
 import com.mediscreen.massessment.bean.PatientBean;
+import com.mediscreen.massessment.model.Assessment;
 import com.mediscreen.massessment.proxy.MNotesProxy;
 import com.mediscreen.massessment.proxy.MPatientProxy;
+import com.mediscreen.massessment.service.AssessmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +23,13 @@ public class AssessmentController {
     private MPatientProxy patientProxy;
     @Autowired
     private MNotesProxy notesProxy;
+    @Autowired
+    private AssessmentService assessmentService;
 
     @GetMapping(value = "/assess/{id}")
-    public ResponseEntity<String> getAssessment(@PathVariable("id") Integer id){
+    public ResponseEntity<Assessment> getAssessment(@PathVariable("id") Integer id){
         PatientBean patient = patientProxy.getPatient(id);
         List<NoteBean> noteBean = notesProxy.getPatHistory(patient.getPatientId());
-        return  new ResponseEntity<>(noteBean.toString(), HttpStatus.OK);
+        return  new ResponseEntity<>(assessmentService.getPatientAssessment(patient, noteBean), HttpStatus.OK);
     }
 }
