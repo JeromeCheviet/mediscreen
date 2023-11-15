281e2936d1bdb19bc40fd413455421c1477b0c4f
Jerome Cheviet <jcheviet@hotmail.com>
2023-11-15 14:01:20 +0100

    UPDATE - test any use case to assess the risk to develop Diabetes

--- microservice-assessment/src/test/java/com/mediscreen/massessment/service/AssessmentServiceTest.java
+++ microservice-assessment/src/test/java/com/mediscreen/massessment/service/AssessmentServiceTest.java
@@ -56,4 +56,219 @@ class AssessmentServiceTest {
         assertEquals(expectedAssessment.toString(), actualAssessment.toString());
     }
 
+    @Test
+    void testGetPatientAssessment_returnRiskNone_whenNoSymptomForYoungerMan() {
+        Risk expectedRisk = Risk.NONE;
+
+        expectedPatientBean.setSex("M");
+        expectedNoteBean.setNotes("Tout va bien");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnRiskNone_whenNoSymptomForYoungerWoman() {
+        Risk expectedRisk = Risk.NONE;
+
+        expectedPatientBean.setSex("F");
+        expectedNoteBean.setNotes("Tout va bien");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnRiskNone_whenNoSymptomForOlderMan() {
+        Risk expectedRisk = Risk.NONE;
+
+        expectedPatientBean.setSex("M");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(40));
+        expectedNoteBean.setNotes("Tout va bien");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnRiskNone_whenNoSymptomForOlderWoman() {
+        Risk expectedRisk = Risk.NONE;
+
+        expectedPatientBean.setSex("F");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(40));
+        expectedNoteBean.setNotes("Tout va bien");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnBorderline_whenTwoSymptomsForOlderMan() {
+        Risk expectedRisk = Risk.BORDERLINE;
+
+        expectedPatientBean.setSex("M");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(40));
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnBorderline_whenTwoSymptomsForOlderWoman() {
+        Risk expectedRisk = Risk.BORDERLINE;
+
+        expectedPatientBean.setSex("F");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(40));
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnInDanger_whenSixSymptomsForOlderMan() {
+        Risk expectedRisk = Risk.DANGER;
+
+        expectedPatientBean.setSex("M");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(40));
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur et son poids est limite");
+        expectedSecondNoteBean.setNotes("Microalbumine en augmentation. Il des sensations de vertige. Taille normal");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+        expectedNoteBeanList.add(expectedSecondNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnInDanger_whenThreeSymptomsForYoungerMan() {
+        Risk expectedRisk = Risk.DANGER;
+
+        expectedPatientBean.setSex("M");
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur et son poids est limite");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnInDanger_whenFourSymptomsForYoungerWoman() {
+        Risk expectedRisk = Risk.DANGER;
+
+        expectedPatientBean.setSex("F");
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur et son poids est limite");
+        expectedSecondNoteBean.setNotes("Microalbumine en augmentation.");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+        expectedNoteBeanList.add(expectedSecondNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnEarlyOnset_whenNineSymptomsForOlderMan() {
+        Risk expectedRisk = Risk.EARLY_ONSET;
+
+        expectedPatientBean.setSex("M");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(40));
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur et son poids est limite. Cholestérol à surveiller.");
+        expectedSecondNoteBean.setNotes("Microalbumine en augmentation. Il des sensations de vertige. Taille normal. ANTICORPS faible, risque de rechute");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+        expectedNoteBeanList.add(expectedSecondNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnEarlyOnset_whenEightSymptomsForOlderWoman() {
+        Risk expectedRisk = Risk.EARLY_ONSET;
+
+        expectedPatientBean.setSex("F");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(40));
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur et son poids est limite. Cholestérol à surveiller.");
+        expectedSecondNoteBean.setNotes("Microalbumine en augmentation. Il des sensations de vertige. Taille normal. ANTICORPS faible.");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+        expectedNoteBeanList.add(expectedSecondNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnEarlyOnset_whenSevenSymptomsForYoungerWoman() {
+        Risk expectedRisk = Risk.EARLY_ONSET;
+
+        expectedPatientBean.setSex("F");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(20));
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur et son poids est limite. Cholestérol à surveiller.");
+        expectedSecondNoteBean.setNotes("Microalbumine en augmentation. Il des sensations de vertige. Taille normal.");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+        expectedNoteBeanList.add(expectedSecondNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
+
+    @Test
+    void testGetPatientAssessment_returnEarlyOnset_whenFiveSymptomsForYoungerMan() {
+        Risk expectedRisk = Risk.EARLY_ONSET;
+
+        expectedPatientBean.setSex("M");
+        expectedPatientBean.setDob(LocalDate.now().minusYears(20));
+        expectedNoteBean.setNotes("Le taux de Hémoglobine A1C est élevé. Le patient est fumeur.");
+        expectedSecondNoteBean.setNotes("Microalbumine en augmentation. Il des sensations de vertige. Taille normal.");
+
+        List<NoteBean> expectedNoteBeanList = new ArrayList<>();
+        expectedNoteBeanList.add(expectedNoteBean);
+        expectedNoteBeanList.add(expectedSecondNoteBean);
+
+        Assessment assessment = assessmentService.getPatientAssessment(expectedPatientBean, expectedNoteBeanList);
+
+        assertEquals(expectedRisk, assessment.getRisk());
+    }
 }
