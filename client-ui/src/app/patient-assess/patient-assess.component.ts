068b9e7470ca81f2c66bb43df2e4c0cc94fe5090
Jerome Cheviet <jcheviet@hotmail.com>
2023-11-19 16:42:04 +0100

    ADD - generate patient-assess component

---
+++ client-ui/src/app/patient-assess/patient-assess.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-assess',
+  templateUrl: './patient-assess.component.html',
+  styleUrls: ['./patient-assess.component.scss']
+})
+export class PatientAssessComponent {
+
+}
